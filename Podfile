c40fbfa064c36a1b550d575e24d5fb143ac96140
Blake Tsuzaki <dtsuzaki@scu.edu>
2017-11-21 19:30:22 -0800

    Added Socket Connection

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TouchRelayTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TouchRelayTest
+  pod 'Starscream'
+
+end
--- TouchRelayTest/ViewController.swift
+++ TouchRelayTest/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import Starscream
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
+    
+    let socket = WebSocket(url: URL(string: "ws://evan.local:8000/x")!, protocols: ["chat"])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        panGestureRecognizer.addTarget(self, action: #selector(ViewController.userDidPan))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        socket.delegate = self
+        socket.connect()
     }
-
-
+    
+    deinit {
+        socket.disconnect(forceTimeout: 0)
+        socket.delegate = nil
+    }
+    
+    @objc func userDidPan(sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: view)
+//        headerLabel.text = "x: \(touchPoint.x), y: \(touchPoint.y)"
+        
+        socket.write(string: "\(max(min(Int(touchPoint.x),255),0))")
+    }
 }
 
+extension ViewController: WebSocketDelegate {
+    func websocketDidConnect(socket: WebSocketClient) {
+        headerLabel.text = "Connected"
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        headerLabel.text = "Disconnected"
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {}
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {}
+}
